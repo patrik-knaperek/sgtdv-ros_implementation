230bfedffa6923bcab66eef7c44ccd30e0962a9c
SGT <dv.sgteam.eu@gmail.com>
2020-09-29 20:01:10 +0200

    Posledne upravy pre posielanie na CAN

--- src/jetson_can_interface/include/JetsonCanInterface.h
+++ src/jetson_can_interface/include/JetsonCanInterface.h
@@ -3,6 +3,10 @@
 //Authors: Juraj Krasňanský
 /*****************************************************/
 
+constexpr const char *NETWORKING_INTERFACE_NAME = "can0"; //"can1"
+constexpr int INVERTOR_BYTES_TO_SEND = 20;//TODO
+constexpr int INVERTOR_MSG_ID = 0x230;
+constexpr int DLCSHIT = 0;//TODO
 
 #include <sgtdv_msgs/Control.h>
 #include <stdio.h>
@@ -19,6 +23,12 @@
 #include <linux/can/raw.h>
 
 
+struct CAN_MSG
+{
+    
+};
+
+
 class JetsonCanInterface
 {
 public:
@@ -26,5 +36,11 @@ public:
     ~JetsonCanInterface();
 
     void Do(const sgtdv_msgs::Control::ConstPtr &msg);
+    void Do(const sgtdv_msgs::Control &msg);
+
 private:
+    int m_socket = -1;
+    struct sockaddr_can m_socketAddress;
+    struct can_frame m_canFrame;
+    struct ifreq m_ifr;
 };
