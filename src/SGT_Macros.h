a1e43e404f9bedf84e70ad3a779b50c6d800508f
ligazetom <krasnanskyjuraj@gmail.com>
2021-09-08 11:02:02 +0200

    Joystick support

--- src/SGT_Macros.h
+++ src/SGT_Macros.h
@@ -6,4 +6,5 @@
 //#define CAMERA_DETECTION_CONSOLE_SHOW
 //#define CAMERA_DETECTION_RECORD_VIDEO
 //#define CAMERA_DETECTION_RECORD_VIDEO_SVO //malo by sa skusit s kamerou
-#define DEBUG_STATE
+#define SGT_DEBUG_STATE
+#define SGT_USE_JOYSTICK
--- src/jetson_can_interface/src/JetsonCanInterfaceNODE.cpp
+++ src/jetson_can_interface/src/JetsonCanInterfaceNODE.cpp
@@ -3,36 +3,92 @@
 //Authors: Juraj Krasňanský
 /*****************************************************/
 
-
+#include "../../SGT_Macros.h"
 #include <ros/ros.h>
 #include "../include/JetsonCanInterface.h"
 #include <sgtdv_msgs/Control.h>
 #include <thread>
 #include <chrono>
+
+
+#ifdef SGT_USE_JOYSTICK
+
+#include <fcntl.h>
+#include <unistd.h>
+
+constexpr int JS_EVENT_BUTTON = 0x01;    /* button pressed/released */
+constexpr int JS_EVENT_AXIS = 0x02;    /* joystick moved */
+constexpr int JS_EVENT_INIT = 0x80;    /* initial state of device */
+constexpr int MAX_AXIS_VALUE = 32767;
+constexpr int MAX_ALLOWED_TORQUE = 10;
+
+struct js_event {
+		__u32 time;     /* event timestamp in milliseconds */
+		__s16 value;    /* value */
+		__u8 type;      /* event type */
+		__u8 number;    /* axis/button number */
+	};
+
+enum ESticks
+{
+    LEFT_LEFT_RIGHT = 0,
+    LEFT_UP_DOWN = 1,
+    RIGHT_LEFT_RIGHT = 3,
+    RIGHT_UP_DOWN = 4
+};
+
+#endif  //SGT_USE_JOYSTICK
+
 int main(int argc, char** argv)
 {
-    JetsonCanInterface jetsonCanInterface;
-    std::thread listenThread;
-
     ros::init(argc, argv, "jetsonCanInterface");
-    ros::NodeHandle handle;
+    JetsonCanInterface jetsonCanInterface;
 
-    //ros::Subscriber pathTrackingSub = handle.subscribe("pathtracking_commands", 1, &JetsonCanInterface::Do, &jetsonCanInterface);
+#ifdef SGT_USE_JOYSTICK
 
-    //std::vector<int> msgIDListenFilter = {0x230};
-
-    //listenThread = std::thread(&JetsonCanInterface::DoListen, msgIDListenFilter);
-    //listenThread.detach();
-
-    //ros::spin();
     sgtdv_msgs::Control control;
 
+    int fd = open ("/dev/input/js1", O_RDONLY);
 
-    while(ros::ok())
-    {
-        std::cin >> control.speed;
-        jetsonCanInterface.Do(control);
+    while (ros::ok())
+    {       
+        struct js_event e;
+	    size_t bytesRead = read (fd, &e, sizeof(e));
+
+        if (bytesRead == sizeof(e) && e.type == JS_EVENT_AXIS)
+        {
+            switch (e.number)
+            {
+                case LEFT_LEFT_RIGHT: break;
+                case LEFT_UP_DOWN:
+                    control.speed = -(e.value / static_cast<float>(MAX_AXIS_VALUE)) * MAX_ALLOWED_TORQUE;
+                    std::cout << control.speed << "\n";
+                    break;
+                case RIGHT_LEFT_RIGHT: break;
+                case RIGHT_UP_DOWN: break;
+                default: break;
+            }
+
+            //jetsonCanInterface.Do(control);
+        }
     }
+
+    close(fd);
+
+#else
+    std::thread listenThread;
+
+    ros::NodeHandle handle;
+
+    ros::Subscriber pathTrackingSub = handle.subscribe("pathtracking_commands", 1, &JetsonCanInterface::Do, &jetsonCanInterface);
+
+    std::vector<int> msgIDListenFilter = {0x230};
+
+    listenThread = std::thread(&JetsonCanInterface::DoListen, msgIDListenFilter);
+    listenThread.detach();
+
+    ros::spin();
+#endif  //SGT_USE_JOYSTICK
 
     return 0;
 }
