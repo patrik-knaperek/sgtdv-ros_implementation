0bdd991355bd4c34ad37177d8e8a77e8b4cf1490
mdudak <mato.dudak@gmail.com>
2022-02-25 15:06:12 +0100

    -filtering lidar data by intensity, axes and KNN -visualizing filtered data -removed opencv dependencies

--- src/lidar_cone_detection/src/LidarConeDetection.cpp
+++ src/lidar_cone_detection/src/LidarConeDetection.cpp
@@ -6,256 +6,131 @@
 
 #include "../include/LidarConeDetection.h"
 
-LidarConeDetection::LidarConeDetection()
-{
-    m_spaceToImgRatio = 100;
-    m_spaceDepth = 8;
-    m_spaceWidth = 9;
-    m_imgWidth = m_spaceWidth * m_spaceToImgRatio;
-    m_imgHeight = m_spaceDepth * m_spaceToImgRatio;
-    m_pointRadius = 3;
-
-    m_image = cv::Mat(m_imgHeight, m_imgWidth, CV_8UC1);
-    m_imgOrigin = cv::Point2i(m_imgWidth / 2., m_imgHeight);
+LidarConeDetection::LidarConeDetection() {
 }
 
-LidarConeDetection::~LidarConeDetection()
-{
-    
+LidarConeDetection::~LidarConeDetection() {
 }
 
-void LidarConeDetection::Do(const sensor_msgs::PointCloud2::ConstPtr& msg)
-{
+void LidarConeDetection::Do(const sensor_msgs::PointCloud2::ConstPtr &msg) {
 #ifdef DEBUG_STATE
     sgtdv_msgs::DebugState state;
     state.workingState = 1;
     m_visDebugPublisher.publish(state);
 #endif
 
-    sgtdv_msgs::Point2DArrPtr coneCoordsMsg(new sgtdv_msgs::Point2DArr);
-    float const *temp;
+    sgtdv_msgs::Point2DArrPtr coneArray(new sgtdv_msgs::Point2DArr);
+    pcl::PCLPointCloud2 *cloud = new pcl::PCLPointCloud2;
+    pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
 
-    m_image = cv::Scalar(255, 255, 255);
-    m_coneCoords.clear();
+    pcl_conversions::toPCL(*msg, *cloud);
 
-    m_coneCoords.reserve(20);       //netusim ci to pomaha opencv ale preco nie
+    pcl::PassThrough<pcl::PCLPointCloud2> passThrough;
+    pcl::RadiusOutlierRemoval<pcl::PCLPointCloud2> radiusRemoval;
 
-    for (int i = 0; i < msg->width; i++)
-    {
-        temp = reinterpret_cast<const float*>(&msg->data[i*msg->point_step]);
-        geometry_msgs::Point point;
+    passThrough.setInputCloud(cloudPtr);
+    if (cloud->width > 0) {
+        //filter data by intensity
+        passThrough.setFilterFieldName("intensity");
+        passThrough.setFilterLimits(CONE_INTENSITY_MIN, CONE_INTENSITY_MAX);
+        passThrough.filter(*cloud);
+    }
+    if (cloud->width > 0) {
+        //filter data by X axis (forward distance from lidar sensor)
+        passThrough.setFilterFieldName("x");
+        passThrough.setFilterLimits(CONE_X_MIN, CONE_X_MAX);
+        passThrough.filter(*cloud);
+    }
+    if (cloud->width > 0) {
+        //filter data by y axis (side distance from lidar sensor)
+        passThrough.setFilterFieldName("y");
+        passThrough.setFilterLimits(CONE_Y_MIN, CONE_Y_MAX);
+        passThrough.filter(*cloud);
+    }
 
-        point.x = *temp;
-        point.y = *(temp + 1);
-        point.z = *(temp + 2);
+    if (cloud->width > 0) {
+        //filter data by z axis (vertical distance from lidar sensor)
+        passThrough.setFilterFieldName("z");
+        passThrough.setFilterLimits(CONE_Z_MIN, CONE_Z_MAX);
+        passThrough.filter(*cloud);
+    }
 
-        if (point.z > 0.1)
-        {
-            DrawPointToImage(cv::Point2d(point.y, point.x));
+    if (cloud->width > 0) {
+        //remove all data, that have less neighbour points than CONE_CLUSTER_NEIGHBOURS in within CONE_CLUSTER_RADIUS
+        radiusRemoval.setInputCloud(cloudPtr);
+        radiusRemoval.setRadiusSearch(CONE_CLUSTER_RADIUS);
+        radiusRemoval.setMinNeighborsInRadius(CONE_CLUSTER_NEIGHBOURS);
+        radiusRemoval.filter(*cloud);
+    }
+
+    //select only one point from points that are within 1m circle
+    if (cloud->width > 2) {
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_xyz(new pcl::PointCloud<pcl::PointXYZ>());
+        pcl::fromPCLPointCloud2(*cloud, *cloud_xyz);
+
+        std::vector<bool> pointSelected(cloud_xyz->size(), false);
+        for (int i = 0; i < cloud_xyz->size(); i++) {
+            for (int j = i + 1; j < cloud_xyz->size(); j++) {
+                if (abs(cloud_xyz->points[i].x - cloud_xyz->points[j].x) < CONE_POINTS_RADIUS &&
+                    abs(cloud_xyz->points[i].y - cloud_xyz->points[j].y) < CONE_POINTS_RADIUS &&
+                    abs(cloud_xyz->points[i].z - cloud_xyz->points[j].z) < CONE_POINTS_RADIUS) {
+                    pointSelected[i] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < pointSelected.size(); i++) {
+            if (pointSelected[i]) {
+                sgtdv_msgs::Point2D point;
+                point.x = cloud_xyz->points[i].x;
+                point.y = cloud_xyz->points[i].y;
+                coneArray->points.push_back(point);
+            }
         }
     }
+    coneArray->points.reserve(cloud->width);
 
-    HoughCircles(m_image, m_coneCoords, CV_HOUGH_GRADIENT, 2, 50., 255, 15, 5, 15); 
-
-    coneCoordsMsg->points.reserve(m_coneCoords.size());
-
-    for(size_t i = 0; i < m_coneCoords.size(); i++)
-    {
-        sgtdv_msgs::Point2D point;
-        cv::Point2d dPoint = getSpaceCoords(cv::Point2i(m_coneCoords[i][0], m_coneCoords[i][1]));
-        point.x = dPoint.y;
-        point.y = dPoint.x;
-
-        coneCoordsMsg->points.push_back(point);
-    }
-
-    m_publisher.publish(coneCoordsMsg);
+    m_publisher.publish(coneArray);
 
 #ifdef DEBUG_STATE
-    state.numOfCones = static_cast<uint32_t>(m_coneCoords.size());
+    state.numOfCones = coneArray->points.size();
     state.workingState = 0;
     m_visDebugPublisher.publish(state);
+    VisualizeData(*coneArray);
 #endif
 
-
-////////////////    VIZUALIZACIA    /////////////////
-
-//     cvtColor(m_image, m_image, CV_GRAY2BGR);
-
-//    for( size_t i = 0; i < m_coneCoords.size(); i++ )
-//     {
-//         cv::Point center(cvRound(m_coneCoords[i][0]), cvRound(m_coneCoords[i][1]));
-//         int radius = cvRound(m_coneCoords[i][2]);
-//         //circle center
-//         circle(m_image, center, 3, cv::Scalar(0,255,0), -1, 8, 0 );
-//         //circle outline
-//         circle(m_image, center, radius, cv::Scalar(0,0,255), 3, 8, 0 );
-//     }
-    
-
-//   cv::imshow("Orig", m_image);
-//   cv::waitKey(0);
-//   getchar();
-
-//   cvtColor(m_image, m_image, CV_BGR2GRAY);
 }
 
-void LidarConeDetection::SetPublisher(ros::Publisher publisher)
-{
+void LidarConeDetection::SetPublisher(ros::Publisher publisher) {
     m_publisher = publisher;
 }
 
-cv::Point2i LidarConeDetection::getImgCoords(const cv::Point2d &spacePoint) const{
-    int imgX, imgY;
+void LidarConeDetection::VisualizeData(const sgtdv_msgs::Point2DArr &point2DArr) {
+    visualization_msgs::MarkerArray markerArray;
+    for (int i = 0; i < point2DArr.points.size(); ++i) {
+        visualization_msgs::Marker marker;
 
-    imgX = m_imgOrigin.x - spacePoint.x * m_spaceToImgRatio;
-    imgY = m_imgOrigin.y - spacePoint.y * m_spaceToImgRatio;
+        marker.ns = "lidar_cone_visualize";
+        marker.id = i;
+        marker.type = visualization_msgs::Marker::CYLINDER;
+        marker.header.frame_id = "velodyne";
+        marker.header.stamp = ros::Time();
 
-    return cv::Point2i(imgX, imgY);
-}
+        marker.pose.position.x = point2DArr.points[i].x;
+        marker.pose.position.y = point2DArr.points[i].y;
 
-cv::Point2d LidarConeDetection::getSpaceCoords(const cv::Point2i &imgPoint) const{
-    double spaceX, spaceY;
+        marker.scale.x = 0.2;
+        marker.scale.y = 0.2;
+        marker.scale.z = 0.2;
 
-    spaceX = -(imgPoint.x - m_imgOrigin.x) / static_cast<double>(m_spaceToImgRatio);
-    spaceY = (m_imgOrigin.y - imgPoint.y) / static_cast<double>(m_spaceToImgRatio);
+        marker.color.r = 1;
+        marker.color.g = 0;
+        marker.color.b = 0;
+        marker.color.a = 1.0;
 
-    return cv::Point2d(spaceX, spaceY);
-}
+        marker.lifetime = ros::Duration(0.1);
 
-bool LidarConeDetection::IsImgPointInImg(const cv::Point2i &imgPoint) const{
-    if(imgPoint.x < 0 || imgPoint.x > m_imgWidth){
-        return false;
+        markerArray.markers.push_back(marker);
     }
-
-    if(imgPoint.y < 0 || imgPoint.y > m_imgHeight){
-        return false;
-    }
-
-    return true;
-}
-
-void LidarConeDetection::DrawPointToImage(const cv::Point2d &spacePoint){
-    cv::Point2i pixel = getImgCoords(spacePoint);
-
-    if(IsImgPointInImg(pixel)){
-        circle(m_image, pixel, m_pointRadius, cv::Scalar(0,255,255), CV_FILLED, 8, 0);
-    }
-    else{
-        return;
-    }
-}
-
-void LidarConeDetection::VisualizeData(const sensor_msgs::PointCloud2::ConstPtr& msg)
-{
-    float const *temp;
-    visualization_msgs::Marker points, cubes;
-    double minDist = 50.;
-
-    m_image = cv::Scalar(255, 255, 255);
-    m_coneCoords.clear();
-    m_coneCoords.reserve(20);
-    
-    points.type = visualization_msgs::Marker::POINTS;
-    cubes.type = visualization_msgs::Marker::LINE_LIST;
-
-    points.id = 0;
-    cubes.id = 1;
-
-    points.header.frame_id = "/my_frame";
-    cubes.header.frame_id = "/my_frame";
-
-    points.header.stamp = ros::Time::now();
-    cubes.header.stamp = ros::Time::now();
-
-    points.ns = "points";
-    cubes.ns = "cubes";
-
-    points.action = visualization_msgs::Marker::ADD;
-    cubes.action = visualization_msgs::Marker::ADD;
-
-    points.pose.orientation.w = 1.0;
-    cubes.pose.orientation.w = 1.0;
-
-    points.scale.x = 0.02;
-    points.scale.y = 0.02;
-
-    points.color.a = 1.0;
-    points.color.r = 0.7;
-    points.color.g = 0.7;
-    points.color.b = 0.7;
-
-    cubes.color.a = 1.0;
-    cubes.color.r = 1.0;
-    
-    cubes.scale.x = 0.2;
-
-    points.points.reserve(msg->width);
-
-    for (int i = 0; i < msg->width; i++)
-    {
-        temp = reinterpret_cast<const float*>(&msg->data[i*msg->point_step]);
-        geometry_msgs::Point point;
-
-        point.x = *temp;
-        point.y = *(temp + 1);
-        point.z = *(temp + 2);
-
-
-       if (point.z > 0.1)
-       {
-            DrawPointToImage(cv::Point2d(point.y, point.x));
-       }
-
-        points.points.push_back(point);
-    }
-
-    HoughCircles(m_image, m_coneCoords, CV_HOUGH_GRADIENT, 2, minDist, 255, 15, 5, 15);
-
-    // cubes.points.reserve(m_coneCoords.size());
-
-    // for(size_t i = 0; i < m_coneCoords.size(); i++)
-    // {
-    //     geometry_msgs::Point point;
-    //     cv::Point2d dPoint = getSpaceCoords(cv::Point2i(m_coneCoords[i][0], m_coneCoords[i][1]));
-    //     point.x = dPoint.y;
-    //     point.y = dPoint.x;
-    //     point.z = 0.;
-
-    //     cubes.points.push_back(point);
-
-    //     point.z += 1.;
-
-    //     cubes.points.push_back(point);
-    // }
-
-    // m_publisher.publish(points);
-
-    // if(cubes.points.size() > 0)
-    // {
-    //     m_publisher.publish(cubes);
-    // }    
-
-    //Will show image with detected cones. You have to close the image window and enter some char to console to continue.
- 
-//     cvtColor(m_image, m_image, CV_GRAY2BGR);
-
-//    for( size_t i = 0; i < m_coneCoords.size(); i++ )
-//     {
-//         cv::Point center(cvRound(m_coneCoords[i][0]), cvRound(m_coneCoords[i][1]));
-//         int radius = cvRound(m_coneCoords[i][2]);
-//         //circle center
-//         circle(m_image, center, 3, cv::Scalar(0,255,0), -1, 8, 0 );
-//         //circle outline
-//         circle(m_image, center, radius, cv::Scalar(0,0,255), 3, 8, 0 );
-//     }
-    
-
-//   cv::imshow("Orig", m_image);
-//   cv::waitKey(0);
-//   getchar();
-
-//   cvtColor(m_image, m_image, CV_BGR2GRAY);
-  
-     
+    m_filteredPointsMarkerPublisher.publish(markerArray);
 }
